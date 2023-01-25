d8db3deafa81e31de4d7b1b56f9b9f551fadbf30
corno <corno.astn.a@schraverus.com>
2023-01-25 01:02:00 +0100

    .

---
+++ test/src/modules/main/implementations/getTestSet.p.ts
@@ -0,0 +1,45 @@
+
+import * as pm from "pareto-core-state"
+import * as pl from "pareto-core-lib"
+
+import * as test from "lib-pareto-test"
+
+import * as api from "../api"
+
+
+import * as pub from "../../../../../pub/dist"
+
+export const $$: api.CgetTestSet = () => {
+
+    pub.$a.call("ls")._execute(($) => {
+        switch ($[0]) {
+            case 'error':
+                pl.cc($[1], ($) => {
+                    pl.logDebugMessage($.stderr)
+                })
+                break
+            case 'success':
+                pl.cc($[1], ($) => {
+                    pl.logDebugMessage($)
+                })
+                break
+            default: pl.au($[0])
+        }
+    })
+
+    const builder = pm.createUnsafeDictionaryBuilder<test.TTestElement>()
+    function createTest(name: string, actual: string, expected: string) {
+        builder.add(name, {
+            type: ["test", {
+                type: ["short string", {
+                    actual: actual,
+                    expected: expected
+                }]
+            }]
+        })
+    }
+
+    return pl.asyncValue({
+        elements: builder.getDictionary()
+    })
+}
