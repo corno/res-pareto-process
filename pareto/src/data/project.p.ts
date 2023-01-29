0765b680fd438cdd9ae281658f4cf276ae8a9e7b
corno <corno.astn.a@schraverus.com>
2023-01-30 00:57:28 +0100

    .

--- pareto/src/data/project.p.ts
+++ pareto/src/data/project.p.ts
@@ -7,12 +7,26 @@ const d = pr.wrapRawDictionary
 import { $ as api } from "./api.p"
 
 export const $: mproject.TProject = {
-    'type': ['resource', {}],
+    'name': "res-pareto-process",
+
+    'author': "Corno",
+    'description': "a pareto wrapper around the process library of NodeJS",
+    'license': "ISC",
+
+    'type': ['resource',  {
+        'devDependencies': d({
+            "@types/node": {},
+        }),
+    }],
     'modules': d({
         "main": {
             'definition': api,
 
         },
     }),
-    'main': "main"
+    'main': "main",
+    'pubdependencies': d({
+        "glo-pareto-common": {},
+    }),
+    'testdependencies': d({}),
 }
