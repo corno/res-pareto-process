8a0c1aa4ecb9f22486e167a5b70c0a48a619e8bd
corno <corno.astn.a@schraverus.com>
2023-03-27 13:44:45 +0200

    .

--- typescript/pub/src/api.generated.ts
+++ typescript/pub/src/api.generated.ts
@@ -3,6 +3,10 @@ import * as pt from 'pareto-core-types'
 import * as g_common from "glo-pareto-common"
 import * as g_this from "./glossary"
 
+export namespace D {
+    
+}
+
 export namespace A {
     
     export type call = () => g_this.ASYNC.A.F.Call
